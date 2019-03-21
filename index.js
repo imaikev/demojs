4ef5a3d15d01cc694e032c4d8db861d5a7a37e85
imaikev <imiguel.villalba@gmail.com>
2019-03-20 21:34:15 -0300

    v6.0

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
+const path = require('path')
+const http = require('http')
 
 app.set('port', (process.env.PORT || 8080))
-app.use(express.static(__dirname + '/dist/angular-animations-site'))
+app.use(express.static(__dirname + '/angular-animations-site/dist/angular-animations-site'));
 
-app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname))
+app.get('/*', function(request, response) {
+  response.sendFile( path.join(__dirname))
 })
 
-app.listen(app.get('port'), function() {
+const server = http.createServer(app);
+
+server.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
